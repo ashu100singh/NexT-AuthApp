d4c3f02b28f98f859bca63f43c6773a49ea93647
Ashutosh Singh <ashutosh100singh100@gmail.com>
2025-12-04 23:47:08 +0530

    User model and signup logic created

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,25 +1,48 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import {axios} from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+
+    const router = useRouter()
 
     const [user, setUser] = React.useState({
         email: "",
         password: "",
         username: ""
     })
+    const [buttonDisabled, setButtonDisabled] = React.useState(true)
+    const [loading, setLoading] = React.useState(false)
 
     const onSignup = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post("/api/users/signup", user)
+            console.log("Signup success", response.data)
+            toast.success("Account created successfully")
+            router.push("/login")
+        } catch (error: any) {
+            console.log("Signup failed", error.message)
+            toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
-
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0 ){
+            setButtonDisabled(false)
+        } else{
+            setButtonDisabled(true)
+        }
+    }, [user])
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-3xl -mt-10 mb-4">SignUp</h1>
+            <h1 className="text-3xl -mt-10 mb-4">{loading ? "Processing" : "Signup"}</h1>
             <div className="space-x-4">
                 <label htmlFor="username">Username</label>
                 <input
@@ -55,9 +78,11 @@ export default function SignupPage() {
             </div> 
             <button 
                 onClick={onSignup}
-                className="py-1 px-4 border border-gray-300 bg-gray-700 hover:bg-gray-900 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                disabled={buttonDisabled}
+                className={`py-1 px-4 border border-gray-300 bg-gray-700 hover:bg-gray-900 rounded-lg mb-4 focus:outline-none focus:border-gray-600
+                    disabled:opacity-70 disabled:cursor-not-allowed disabled:hover:bg-gray-700`}
             >
-                SignUp
+                Sign Up
             </button>
             <p>Already have an account? <Link href="/login" className="text-blue-500">Login here</Link></p>
         </div>
